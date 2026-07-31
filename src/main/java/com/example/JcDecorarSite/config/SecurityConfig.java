58dd5b77768b37215eef2742b798012836d21228
andre felipe <andre.felipe106@icloud.com>
2026-07-31 00:22:57 -0300

    teste filtro

--- src/main/java/com/example/JcDecorarSite/config/SecurityConfig.java
+++ src/main/java/com/example/JcDecorarSite/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
+                
 
                 .sessionManagement(session -> session.sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS))
