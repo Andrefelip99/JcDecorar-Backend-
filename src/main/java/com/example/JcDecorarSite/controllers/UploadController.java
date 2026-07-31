1236fb632a8a1ed02c472c983fe0384b7e27364b
andre felipe <andre.felipe106@icloud.com>
2026-07-31 11:51:17 -0300

    test 9

--- src/main/java/com/example/JcDecorarSite/controllers/UploadController.java
+++ src/main/java/com/example/JcDecorarSite/controllers/UploadController.java
@@ -1,7 +1,5 @@
 package com.example.JcDecorarSite.controllers;
 
-import java.util.Map;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.JcDecorarSite.services.CloudinaryService;
 
+
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
@@ -20,33 +19,9 @@ public class UploadController {
     private final CloudinaryService cloudinaryService;
 
 
-    public UploadController(
-            CloudinaryService cloudinaryService) {
-
+    public UploadController(CloudinaryService cloudinaryService) {
         this.cloudinaryService = cloudinaryService;
     }
-
-
-/* 
-    @PostMapping
-    public ResponseEntity<Map<String, String>> upload(
-            @RequestParam("file") MultipartFile file) {
-
-
-        String imageUrl =
-                cloudinaryService.uploadImage(file);
-
-
-        return ResponseEntity.ok(
-                Map.of(
-                    "url",
-                    imageUrl
-                )
-        );/* */
-
-       
-
-
 
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -64,7 +39,7 @@ public class UploadController {
         String url = cloudinaryService.uploadImage(file);
 
 
-        System.out.println("URL CLOUDINARY: " + url);gi
+        System.out.println("URL CLOUDINARY: " + url);
 
 
         return ResponseEntity.ok(url);
